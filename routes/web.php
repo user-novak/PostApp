6fb35a81cdf5d21ec86191e571a64a36fe856900
Novak Zecevich <novakmzv@gmail.com>
2022-10-25 09:55:01 -0500

    añadiendo auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
